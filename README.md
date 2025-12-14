5978c26f8ffebbde41979cbd256bdf5d7fbe250d
Stark-light <craftsylaviana192002@gmail.com>
2025-12-15 03:31:50 +0530

    commited

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Terraform-AWS-VPC-EC2-Project
 Terraform project that provisions an AWS VPC, subnet, security group, and EC2 instance using Infrastructure-as-Code.
+
+## Resources Created
+- VPC
+- Public Subnet
+- Internet Gateway
+- Route Table
+- Security Group (SSH access)
+- EC2 Instance (Free Tier eligible)
+
+## Prerequisites
+- AWS account
+- AWS CLI configured
+- Terraform installed
+- EC2 Key Pair created in AWS
+
+## Steps to Run
+1. terraform init
+2. terraform plan
+3. terraform apply
+
+## Purpose
+This project was created for learning Infrastructure-as-Code and basic cloud networking concepts.
+
---
+++ main.tf
@@ -0,0 +1,70 @@
+# VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+# Route Table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+# Security Group
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance (Free Tier)
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  key_name      = var.key_name
+
+  tags = {
+    Name = "terraform-ec2"
+  }
+}
